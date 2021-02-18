f876cdfcf58a711946fbae9e8b050d5aae81ee27
Payal Singhl <payal.singhal@webappmate.com>
2021-02-18 09:58:46 -0500

    initial commit

---
+++ app/code/Webappmate/Popup/Block/Location.php
@@ -0,0 +1,16 @@
+<?php
+namespace Webappmate\Popup\Block;
+
+class Location extends \Magento\Framework\View\Element\Template
+{
+    /**
+     * Retrieve form action url and set "secure" param to avoid confirm
+     * message when we submit form from secure page to unsecure
+     *
+     * @return string
+     */
+    public function getFormActionUrl()
+    {
+        return true;
+    }
+}
